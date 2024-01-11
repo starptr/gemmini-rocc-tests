6c89e7d79e815f7b2a6e4b30c3e704c7034bd4dd
metalift Demo <metalift@a3.Millennium.Berkeley.EDU>
2024-01-11 05:34:00 -0800

    Add nB8

---
+++ bareMetalC/gaudi_normalBlend8.c
@@ -0,0 +1,70 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef BAREMETAL
+#include <sys/mman.h>
+#endif
+#include "include/gemmini_testutils.h"
+#define LEN 16
+
+elem_t muldiv255(elem_t a, elem_t b) {
+    return (a * b) / 255;
+}
+
+void naive_normalBlend8(elem_t base[LEN][LEN], elem_t active[LEN][LEN], elem_t opacity, elem_t out[LEN][LEN]) {
+    for (int i = 0; i < LEN; i++) {
+        out[0][i] = muldiv255(opacity, active[0][i]) + muldiv255(255 - opacity, base[0][i]);
+    }
+}
+
+void runner(elem_t base[LEN][LEN], elem_t active[LEN][LEN], elem_t opacity, elem_t out[LEN][LEN]) {
+    const elem_t* ao = GEMMINI_ACC_SCALE(active, opacity);
+    const elem_t* ao2 = GEMMINI_ACC_SCALE(ao, 0.0039215686);
+    const elem_t* bo = GEMMINI_ACC_SCALE(base, 1 - opacity);
+    const elem_t* bo2 = GEMMINI_ACC_SCALE(bo, 0.0039215686);
+    tiled_resadd_auto(
+        LEN, LEN,
+        opacity, 1 - opacity,
+        1,
+        active[0], base[0],
+        out[0],
+        false,
+        WS
+    );
+}
+int main() {
+#ifndef BAREMETAL
+    if (mlockall(MCL_CURRENT | MCL_FUTURE) != 0) {
+      perror("mlockall failed");
+      exit(1);
+    }
+#endif
+
+  printf("Size of DIM: %d\n", DIM);
+  printf("Flush Gemmini TLB of stale virtual addresses\n");
+  gemmini_flush(0);
+
+  static elem_t Left[LEN][LEN];
+  static elem_t Right[LEN][LEN];
+  static elem_t Out[LEN][LEN];
+  static elem_t opacity = 0.3;
+  for (int i = 0; i < LEN; i++) {
+    Left[0][i] = i;
+    Right[0][i] = i;
+  }
+
+  uint64_t start_cpu = read_cycles();
+  naive_normalBlend8(Left, Right, opacity, Out);
+  uint64_t end_cpu = read_cycles();
+  printf("CPU conv took %llu cycles\n", end_cpu - start_cpu);
+
+  uint64_t start_g = read_cycles();
+  runner(Left, Right, opacity, Out);
+  uint64_t end_g = read_cycles();
+  printf("Hardware conv took %llu cycles\n", end_g - start_g);
+
+  //print1d(Out);
+  exit(0);
+}
