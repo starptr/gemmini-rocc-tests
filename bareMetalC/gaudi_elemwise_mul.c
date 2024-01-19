205e733a8078d194d03837ae29f30d00a770c9a3
metalift Demo <metalift@a3.Millennium.Berkeley.EDU>
2024-01-19 02:08:31 -0800

    Add elemwise_mul 1d

---
+++ bareMetalC/gaudi_elemwise_mul.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef BAREMETAL
+#include <sys/mman.h>
+#endif
+#include "include/gemmini_testutils.h"
+#define LEN 16
+
+void runner(elem_t left[LEN][LEN], elem_t right[LEN][LEN], elem_t out[LEN][LEN]) {
+  for (int i = 0; i < LEN; i++) {
+    out[0][i] = left[0][i] * right[0][i];
+  }
+}
+int main() {
+#ifndef BAREMETAL
+    if (mlockall(MCL_CURRENT | MCL_FUTURE) != 0) {
+      perror("mlockall failed");
+      exit(1);
+    }
+#endif
+
+  printf("Size of DIM: %d\n", DIM);
+  printf("Flush Gemmini TLB of stale virtual addresses\n");
+  gemmini_flush(0);
+
+  static elem_t Left[LEN][LEN];
+  static elem_t Right[LEN][LEN];
+  static elem_t Out[LEN][LEN];
+  int v = 0;
+  for (int i = 0; i < LEN; i++) {
+    for (int j = 0; j < LEN; j++) {
+      Left[i][j] = v;
+      Right[i][j] = v;
+      v++;
+    }
+  }
+
+  uint64_t start_g = read_cycles();
+  runner(Left, Right, Out);
+  uint64_t end_g = read_cycles();
+  printf("Hardware conv took %llu cycles\n", end_g - start_g);
+
+  //print1d(Out);
+  exit(0);
+}
